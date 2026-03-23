2f8bbb163509b25dae2389af764a7dae9bead93e
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2026-03-24 01:13:12 +0200

    Add TypeScript features including type aliases, enums, and stricter compiler options

--- typescript/Practice/dist/main.js
+++ typescript/Practice/dist/main.js
@@ -24,4 +24,64 @@ function sum(...numbers) {
 }
 console.log(sum(1, 2, 3, +true));
 console.log(sum(4, 5, 6, 7, 8.5));
+let myString = "This is a string using type alias.";
+console.log(myString);
+let value = "A string or a number";
+console.log(value);
+value = 42;
+console.log(value);
+function getAction(button) {
+    const actions = {
+        up: "Move Up",
+        down: "Move Down",
+        right: "Move Right",
+        left: "Move Left",
+    };
+    return actions[button];
+}
+console.log(getAction("up"));
+console.log(getAction("down"));
+console.log(getAction("right"));
+console.log(getAction("left"));
+let compare = (n, b) => {
+    if (n < b)
+        return -1;
+    if (n > b)
+        return 1;
+    return 0;
+};
+console.log(compare(5, 10));
+console.log(compare(10, 5));
+console.log(compare(7, 7));
+let myNumber = -1;
+console.log(myNumber);
+let article = [11, "Title One", true];
+console.log(article);
+article = [12, "Title Two", false];
+console.log(article);
+const [id, title, published] = article;
+console.log(id);
+console.log(title);
+console.log(published);
+function logMessage(message) {
+    console.log(message);
+}
+logMessage("This is a log message.");
+var Level;
+(function (Level) {
+    Level[Level["Kids"] = 15] = "Kids";
+    Level[Level["Easy"] = 9] = "Easy";
+    Level[Level["Medium"] = 6] = "Medium";
+    Level[Level["Hard"] = 3] = "Hard";
+})(Level || (Level = {}));
+let lvl = "Medium";
+if (lvl === "Medium") {
+    console.log(`The Level Is ${lvl} And Number Of Seconds Is ${Level[lvl]}`);
+}
+let user = {
+    name: "Alice",
+    age: 25,
+    isAdmin: true,
+};
+console.log(user);
 //# sourceMappingURL=main.js.map
--- typescript/Practice/main.ts
+++ typescript/Practice/main.ts
@@ -44,3 +44,120 @@ function sum(...numbers: number[]): number {
 console.log(sum(1, 2, 3, +true)); // 6
 console.log(sum(4, 5, 6, 7, 8.5)); // 30
 
+// Type Alias
+type ts = string;
+let myString: ts = "This is a string using type alias.";
+console.log(myString);
+
+type stAndNum = string | number;
+let value: stAndNum = "A string or a number";
+console.log(value);
+value = 42;
+console.log(value);
+
+type buttons = {
+  up: string;
+  down: string;
+  right: string;
+  left: string;
+};
+
+function getAction(button: keyof buttons): string {
+  const actions: buttons = {
+    up: "Move Up",
+    down: "Move Down",
+    right: "Move Right",
+    left: "Move Left",
+  };
+
+  return actions[button];
+}
+
+console.log(getAction("up"));
+console.log(getAction("down"));
+console.log(getAction("right"));
+console.log(getAction("left"));
+
+// Data Types => string, number, boolean, null, undefined, symbol, bigint, object, array, tuple, enum, any, void, never, unknown
+// Literal Types
+
+type nums = -1 | 0 | 1;
+
+let compare = (n: number, b: number): nums => {
+  if (n < b) return -1;
+  if (n > b) return 1;
+  return 0;
+};
+
+console.log(compare(5, 10)); // -1
+console.log(compare(10, 5)); // 1
+console.log(compare(7, 7)); // 0
+
+// let myNumber: nums = 5; // Error: Type '5' is not assignable to type 'nums'.
+let myNumber: nums = -1;
+console.log(myNumber);
+
+// Tuple.
+let article: readonly [number, string, boolean] = [11, "Title One", true];
+console.log(article);
+
+article = [12, "Title Two", false];
+// article.push(100); // Error: Property 'push' does not exist on type 'readonly [number, string, boolean]'.
+console.log(article);
+
+const [id, title, published] = article;
+console.log(id);
+console.log(title);
+console.log(published);
+
+// Void & Never
+function logMessage(message: string): void {
+  console.log(message);
+}
+
+logMessage("This is a log message.");
+
+// function throwError(message: string): never {
+//   throw new Error(message);
+// }
+
+// try {
+//   throwError("This is an error message.");
+// } catch (error) {
+//   console.error(error);
+// }
+
+enum Level {
+  Kids = 15,
+  Easy = 9,
+  Medium = Easy - 3,
+  Hard = 3,
+}
+
+let lvl: string = "Medium";
+
+if (lvl === "Medium") {
+  console.log(
+    `The Level Is ${lvl} And Number Of Seconds Is ${Level[lvl as keyof typeof Level]}`,
+  );
+}
+
+// TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
+
+// Type Annotations With Objects
+interface User {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+}
+
+let user: User = {
+  name: "Alice",
+  age: 25,
+  isAdmin: true,
+};
+
+console.log(user);
+
+// ? => Optional Property
+// Readonly => Cannot Reassign The Value Of The Property After Initialization
